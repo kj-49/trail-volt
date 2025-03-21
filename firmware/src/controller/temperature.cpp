b89456baf1b802657a98ba788f2a6aa2dd285c20
Kyle James <ksjames49@gmail.com>
2025-03-21 08:05:47 -0600

    Add remaining thermistor code

--- firmware/src/controller/temperature.cpp
+++ firmware/src/controller/temperature.cpp
@@ -4,12 +4,11 @@
 #include <math.h>
 #include <stdint.h>
 
-#define ADC_MAX   1023.0f   // Arduino Nano has a 10-bit ADC (0-1023)
-
-typedef uint16_t u16; // Use unsigned 16 bit readings for NANO
-
-
-float get_resistance(int pin) {
+/**
+ * @brief  Gets the resistance by reading the voltage drop across the thermistor.
+ * @return The resistance in Ohms.
+ */
+static float get_resistance(int pin) {
     float voltage = read_from_adc(pin, THERMISTOR_ADC_DIVISION);
 
     float drop;
@@ -22,37 +21,18 @@ float get_resistance(int pin) {
     float resistance = (5.0f * SERIES_RESISTOR) / drop;
 }
 
-float temperature_get_series_reading() {
-    float resistance = get_resistance(THERMISTOR_2_PIN);
-
-    /*
-     * Yet to be implemented.
-     */
-
-    return 0;
-}
-
-float temperature_get_ground_reading() {
-    float resistance = get_resistance(THERMISTOR_1_PIN);
-
-    /*
-     * Yet to be implemented.
-     */
-     
-    return 0;
-}
-
-// The below function returns the temp. of thermistor based on the varying resistance.
-
-float getTempfromThermistor(int pin) {
+/**
+ * @brief  Gets the temperature assuming a thermistor is attached to the given pin.
+ * @param  pin: The ADC pin attached to the thermistor.
+ * @return The temperature in C.
+ */
+static float get_temp_from_thermistor(int pin) {
     const float R0 = 10000.0f;
     const float R_fixed = 40000.0f;
     const float Beta = 3380.0f;            // These were found in datasheet.
     const float T0 = 298.15f;
     
-    float r_ntc = get_resistance(pin);        // This seems more abstract; less clutter for code at the expense of less modularity.
-    
-    if (adc_value <= 0 || adc_value >= ADC_MAX) return -999.0f; // Use this right away to maintain 'safe' bounds - can adjust if needed. I set ADC_MAX to 5.
+    float r_ntc = get_resistance(pin);
 
     // Use a Steinhart equation for conversions.
     float steinhart = (1.0f / T0) + (1.0f / Beta) * log(r_ntc / R0);
@@ -60,19 +40,18 @@ float getTempfromThermistor(int pin) {
     float t_celsius = t_kelvin - 273.15f;
     
     return t_celsius;
-
 }
 
-void testTemperature(float t_celsius){
-    int lowerBound = 15;
-    int higherBound = 40;
-    if(t_celsius > lowerBound && u32_tempK < t_celsius){
-        PORTA.OUT |= 0x01;
-    }
-    else{
-        PORTA.OUT &= ~(0x01);
-    }
+float temperature_get_series_reading() {
+    float temp_c = get_temp_from_thermistor(THERMISTOR_2_PIN);
+    return temp_c;
 }
+
+float temperature_get_ground_reading() {
+    float temp_c = get_temp_from_thermistor(THERMISTOR_1_PIN);
+    return temp_c;
+}
+
 
 
 
