167761f75e45d72c0bb12e0c8b85af2702f1990f
Kyle James <ksjames49@gmail.com>
2025-03-25 19:46:09 -0600

    Fix adc call

--- firmware/src/controller/temperature.cpp
+++ firmware/src/controller/temperature.cpp
@@ -9,7 +9,7 @@
  * @return The resistance in Ohms.
  */
 static float get_resistance(int pin) {
-    float voltage = read_from_adc(pin, THERMISTOR_ADC_DIVISION);
+    float voltage = adc_read(pin, THERMISTOR_ADC_DIVISION);
 
     float drop;
     if (IS_HIGH_SIDE) {
